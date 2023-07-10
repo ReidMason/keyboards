6b14e1abf24b2f09ac0646562c306012fceee68d
Reid Mason <maddogshain132@gmail.com>
2023-07-10 23:01:00 +0100

    Increased tapping term

--- Kyria/ReidMason/config.h
+++ Kyria/ReidMason/config.h
@@ -40,6 +40,6 @@
 #define LED_PIN_ON_STATE 0
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 150
+#define TAPPING_TERM 200
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
